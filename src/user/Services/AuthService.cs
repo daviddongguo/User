aaa6e522566fcee6db5a4d1bc8fe53de73884bdb
daviddongguo <david.dong.guo@gmail.com>
2020-11-03 13:31:53 -0500

    Add AuthService and it's tests

--- src/user/Services/AuthService.cs
+++ src/user/Services/AuthService.cs
@@ -1,6 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using user.Models;
 
 namespace user.Services
@@ -8,20 +15,117 @@ namespace user.Services
     public class AuthService
     {
         private readonly UserDbContext _context;
-
-        public AuthService(UserDbContext context)
+        private readonly IConfiguration _configuration;
+        public AuthService(UserDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
-
-        public async Task<IEnumerable<User>> GetAllUsers()
+        public async Task<IQueryable<User>> GetAllUsers(int pageSize = 5, int page = 1)
         {
-            return await _context.Users.ToListAsync();
+            return (await _context.Users
+                .OrderBy(u => u.Email)
+                .OrderBy(u => u.Name)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync())
+                .AsQueryable();
+        }
+        public async Task<User> GetUserByEmail(string email)
+        {
+           return await _context.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+        }
+        public async Task<bool> IsUserExisted(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
         }
 
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+            if (dbUser == null)
+            {
+                return new ServiceResponse<string>
+                {
+                    Success = false,
+                    Message = $"{email} has not been found!",
+                };
+            }
+            if (!VerifyPasswordHash(password, dbUser.PasswordHash, dbUser.PasswordSalt))
+            {
+                return new ServiceResponse<string>
+                {
+                    Success = false,
+                    Message = $"email or password is wrong!",
+                };
+            }
+            return new ServiceResponse<string>
+            {
+                Data = CreateToken(dbUser),
+            };
+        }
+        public async Task<ServiceResponse<string>> Register(User toSaveUser, string password)
+        {
+            if (await IsUserExisted(toSaveUser.Email))
+            {
+                return new ServiceResponse<string>
+                {
+                    Success = false,
+                    Message = $"{toSaveUser.Email} already exists!",
+                };
+            };
+            Utility.CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            toSaveUser.PasswordHash = passwordHash;
+            toSaveUser.PasswordSalt = passwordSalt;
+            await _context.Users.AddAsync(toSaveUser);
+            await _context.SaveChangesAsync();
+            return new ServiceResponse<string>
+            {
+                Data = toSaveUser.Id,
+            };
+        }
 
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        public string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
 
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("MySettings:Token").Value)
+            );
 
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
--- test/LocalDbUserserviceTest.cs
+++ test/LocalDbUserserviceTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
+using user.Models;
 using user.Services;
 
 namespace test
@@ -11,24 +13,70 @@ namespace test
         [SetUp]
         public void SetUp()
         {
-            _service = new AuthService(new LocalInMemoryDbContextFactory().GetUserContext());
+            _service = new AuthService(new LocalInMemoryDbContextFactory().GetUserContext(), ConfigurationManager.AppSetting);
         }
 
-        [Test]
-        public void GetAllUsers()
+        [TestCase(5, 1)]
+        [TestCase(10, 1)]
+        [TestCase(8, 2)]
+        public void GetAllUsers(int pageSize, int page)
         {
             //
-            var result = _service.GetAllUsers().GetAwaiter().GetResult();
+            var result = _service.GetAllUsers(pageSize, page).GetAwaiter().GetResult();
+            Assert.That(result.Count() == pageSize);
 
+            int i = 1;
             foreach (var item in result)
             {
-                System.Console.WriteLine(item.Name);
-
+                System.Console.WriteLine($"{i++}  :  Name: {item.Name}, Id: {item.Id}, IsActive: {item.IsActive} ");
             }
+        }
 
+        [TestCase()]
+        public void GetAllUsers_Default()
+        {
+            var result = _service.GetAllUsers().GetAwaiter().GetResult();
+            Assert.That(result.Count() == 5);
+
+            int i = 1;
+            foreach (var item in result)
+            {
+                System.Console.WriteLine($"{i++}  :  Name: {item.Name}, Id: {item.Id}, IsActive: {item.IsActive} ");
+            }
+        }
+
+        [TestCase("sis@user.com", true)]
+        [TestCase("Sis@user.com", true)]
+        [TestCase("Sis@xxer.com", false)]
+        public void GetUserByEmail(string email, bool expectedValue)
+        {
+            var result = _service.GetUserByEmail(email).GetAwaiter().GetResult();
+            Assert.That(result !=null, Is.EqualTo(expectedValue));
+
+            System.Console.WriteLine($"{email} : {result?.Email}");
+        }
+
+        [TestCase("sis@user.com", true)]
+        [TestCase("Sis@user.com", true)]
+        [TestCase("Sis@xxer.com", false)]
+        public void IsUserExisted(string email, bool expectedValue)
+        {
+            var result = _service.IsUserExisted(email).GetAwaiter().GetResult();
+            Assert.That(result == expectedValue);
+
+            System.Console.WriteLine($"{email} : {result}");
+        }
+
+        [TestCase("sis@user.com")]
+        public void CreateToken(string email)
+        {
+            var result = _service.CreateToken(_service.GetUserByEmail(email).GetAwaiter().GetResult());
             Assert.That(result, Is.Not.Null);
 
+            System.Console.WriteLine(result);
         }
+
+
 
     }
 }
