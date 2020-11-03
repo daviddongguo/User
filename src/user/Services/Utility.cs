e7e3e77056075e95427652b5a25ff1ea73be2e81
daviddongguo <david.dong.guo@gmail.com>
2020-11-03 00:14:06 -0500

    Add CreatePassword and RandomString method in Utility

---
+++ src/user/Services/Utility.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text;
+
+namespace user.Services
+{
+    public static class Utility
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static string RandomString(int length)
+        {
+            const string pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var builder = new StringBuilder();
+            var rnd = new Random();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[rnd.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+
+    }
+}
