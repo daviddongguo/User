64c58091a5a9c743f2648e0dc580097d4c212137
daviddongguo <david.dong.guo@gmail.com>
2020-11-03 21:11:06 -0500

    All tests passed

---
+++ src/user/AutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using user.Services;
+
+namespace user
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AuthService>()
+                .As<IAuthService>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
