98e532f64c7ecd535c5d5ef5018cd70ec758e2d2
daviddongguo <david.dong.guo@gmail.com>
2020-11-03 21:09:40 -0500

    Add services.AddDbContext Add services.AddScoped

--- src/user/Program.cs
+++ src/user/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +15,12 @@ namespace user
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //var host = Host.CreateDefaultBuilder(args)
+            //    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            //    .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder.UseStartup<Startup>())
+            //    .Build();
+
+            //await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- src/user/Startup.cs
+++ src/user/Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using user.Services;
 
 namespace user
 {
@@ -22,9 +19,17 @@ namespace user
 
         public IConfiguration Configuration { get; }
 
+        //public void ConfigureContainer(ContainerBuilder builder)
+        //{
+        //    builder.RegisterModule(new AutofacModule());
+        //}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UserDbContext>(x => x.UseMySql(Configuration.GetConnectionString("MySqlConnection")));
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IAuthService, AuthService>();
             services.AddControllers();
         }
 
