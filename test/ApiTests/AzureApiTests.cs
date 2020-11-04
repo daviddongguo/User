3860bd2cd635a47478e0274276a9e90093bab129
daviddongguo <david.dong.guo@gmail.com>
2020-11-04 00:16:17 -0500

    Add test project for testing online api

--- test/ApiTests/AzureApiTests.cs
+++ test/ApiTests/AzureApiTests.cs
@@ -1,18 +1,135 @@
 using NUnit.Framework;
+using RestSharp;
+using user.Dtos;
 
 namespace apiTests
 {
+    [TestFixture]
     public class AzureApiTests
     {
+        private RestClient _client;
+        //private readonly string baseUrl = "https://david-user-login.azurewebsites.net/";
+        private readonly string baseUrl = "http://localhost:5000";
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            _client = new RestClient(baseUrl);
+            _client.AddHandler("application/json", () => new RestSharp.Serialization.Json.JsonSerializer());
+            _client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(200), Timeout(8000)]
+        public void TestUsers(int expectedstatusCode)
         {
-            Assert.Pass();
+            // Arrange
+            var request = new RestRequest("auth/test", Method.GET);
+
+            // Act
+            var response = _client.ExecuteGetAsync(request).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.That((int)response.StatusCode == expectedstatusCode);
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
         }
+
+        [TestCase("aaabbddddddd", 204)]
+        [TestCase("sis@user.com", 200)]
+        [TestCase("Sis@user.com", 200)]
+        [TestCase("Sis@xxer.com", 204)]
+        public void IsUserExisted(string email, int expectedStatusCode)
+        {
+            // Arrange
+            var request = new RestRequest("auth/IsEmailExisted/" + email, Method.GET);
+
+            // Act
+            var response = _client.ExecuteAsync(request).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.That((int)response.StatusCode == expectedStatusCode);
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
+        }
+
+        [TestCase("sis@user.com", "123", 200)]
+        [TestCase("sis@user.com", "133", 400)]
+        [TestCase("sis@usxx.com", "123", 400)]
+        public void Login(string email, string password, int expectedStatusCode)
+        {
+            // Arrange
+            var request = new RestRequest("auth/login" , Method.POST);
+            request.AddJsonBody(new UserLoginDto
+            {
+                Email = email,
+                Password = password
+            });
+
+            // Act
+            var response = _client.ExecuteAsync(request).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.That((int)response.StatusCode == expectedStatusCode);
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
+        }
+
+        [TestCase("sis@user.com", "323", 400)]
+        public void Register_WhenFailed(string email, string password, int expectedStatusCode)
+        {
+            // Arrange
+            var request = new RestRequest("auth/Register", Method.POST);
+            request.AddJsonBody(new UserRegisterDto
+            {
+                Email = email,
+                Password = password
+            });
+
+            // Act
+            var response = _client.ExecuteAsync(request).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.That((int)response.StatusCode == expectedStatusCode);
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
+        }
+
+        [TestCase("guest@test.com", "323", 201)]
+        public void Register_WhenSuccess(string email, string password, int expectedStatusCode)
+        {
+            // Arrange
+            var request = new RestRequest("auth/Register", Method.POST);
+            request.AddJsonBody(new UserRegisterDto
+            {
+                Email = email,
+                Password = password
+            });
+
+            // Act
+            var response = _client.ExecuteAsync(request).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.That((int)response.StatusCode == expectedStatusCode);
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
+
+            // DeleteRT
+            var id = response.Content;
+            request = new RestRequest("auth/" + id.Substring(1,12), Method.DELETE);
+            response = _client.ExecuteAsync(request).GetAwaiter().GetResult();
+            System.Console.WriteLine(response.ResponseUri);
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content);
+
+
+        }
+
+
+
     }
 }
